69ef8e2225959c5dd48b6a6cb29cacfea6335e3f
Tyler Kelley <tylerckelley@gmail.com>
2013-07-16 15:33:23 -0400

    Firsties!

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html>
+
+<head>
+
+  <title>Sign up for your Github account</title>
+  <link rel="stylesheet" type="text/css" href="signup.css" />
+
+</head>
+
+<body>
+
+<header>
+  <div class="wrapper">
+
+    <h1>WDDBS + GITHUB</h1>
+    <h2>18 Months of private repo's for current WDDBS students <sup>*</sup></h2>
+    <img src="img/octokitty.png" alt="OctoKitty!" />
+
+  </div>
+</header>
+
+<div class="cf">&nbsp;</div>
+
+<section>
+  <div class="wrapper">
+
+    <form action="get" method="something">
+
+      <div class="input">
+        <input type="text" name="name" placeholder="Full Name" />
+        <div class="validate fail">Whoops!</div>
+        <div class="validate pass">Hooray!</div>
+      </div>
+
+      <div class="input">
+        <input type="text" name="email" placeholder="Full Sail Email" />
+        <div class="validate fail">Whoops!</div>
+        <div class="validate pass">Hooray!</div>
+      </div>
+
+      <div class="input">
+        <input type="text" name="user" placeholder="Github Username" />
+        <div class="validate fail">Whoops!</div>
+        <div class="validate pass">Hooray!</div>
+      </div>
+
+      <div class="input">
+        <input type="password" name="password" placeholder="Github Password" />
+        <div class="validate fail">Whoops!</div>
+        <div class="validate pass">Hooray!</div>
+      </div>
+
+      <div class="input">
+        <input type="submit" value="Sign me up!" />
+      </div>
+
+    </form>
+
+  </div>
+</section>
+
+<div class="cf">&nbsp;</div>
+
+<footer>
+  <div class="wrapper">
+
+    <p>* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ullamcorper lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet cursus massa, eu lacinia nunc tempor ut.</p>
+    <img src="img/fslogo_grey.png" />
+
+  </div>
+</footer>
+
+</body>
+</html>
