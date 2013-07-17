279d9330f86962b2b25dee00975c80ad99f46599
Chris Chapman <christopher.chapman@me.com>
2013-07-17 04:08:00 -0400

    Added Github Oauth2 Login. Simplified Form

---
+++ inc/js/main.js
@@ -0,0 +1,19 @@
+var fb = new Firebase('https://fsgithub.firebaseio.com/');
+var auth = new FirebaseSimpleLogin(fb, function(error, user) {
+  if (error) {
+    // an error occurred while attempting login
+    console.log(error);
+  } else if (user) {
+    // user authenticated with Firebase
+    console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+  } else {
+    // user is logged out
+  }
+});
+
+var fire = function(){
+	auth.login('github', {
+		rememberMe: true,
+  		scope: 'user,repo,gist'
+	});
+}
